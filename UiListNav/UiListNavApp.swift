9f37cfd333cf8cf5f04ce376de6f57cb44a94280
Nij Mehar <nijmehar16@gmail.com>
2022-11-29 11:35:42 +0530

    Initial Commit

---
+++ UiListNav/UiListNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  UiListNavApp.swift
+//  UiListNav
+//
+//  Created by Nij Mehar on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UiListNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
